f5382ca181049e4ee52873484fab66b5f689dd55
avelitj12 <avelitj12@gmail.com>
2025-05-17 13:50:37 -0400

    Set up world light and canvas modulate for darkness

--- SCRIPTS/playground.gd
+++ SCRIPTS/playground.gd
@@ -1,5 +1,8 @@
 extends Level
 
+@onready var world_light: DirectionalLight2D = $WorldLight
+
 func _level_ready():
 	await get_tree().create_timer(1).timeout
 	Globals.update_info("Updated")
+	world_light.energy = .2
