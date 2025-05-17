2e4a8b4a254f1951bd910e0de9fa08db7c79e458
avelitj12 <avelitj12@gmail.com>
2025-05-17 13:41:04 -0400

    Player netting attack recognizing frog

--- SCRIPTS/player.gd
+++ SCRIPTS/player.gd
@@ -1,15 +1,43 @@
 extends CharacterBody2D
 
+class_name Player
+
+@onready var net_attack_area: Area2D = $NetAttackArea
+@onready var net_collider: CollisionShape2D = $NetAttackArea/CollisionShape2D
+@onready var attack_time: Timer = $NetAttackArea/AttackTime
 
 const SPEED = 300.0
 
+var attacking := false
+
+func _ready() -> void:
+	net_attack_area.hide()
 
 func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_vector("left", "right", "up", "down").normalized()
-	velocity = direction * SPEED
-	if direction:
-		rotation = -direction.angle_to(Vector2.UP)
+	if not attacking:
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_vector("left", "right", "up", "down").normalized()
+		velocity = direction * SPEED
+		if direction:
+			rotation = -direction.angle_to(Vector2.UP)
 
-	move_and_slide()
+		move_and_slide()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("net") and attacking == false:
+		_use_net()
+
+func _use_net():
+	net_attack_area.show()
+	net_collider.disabled = false
+	attack_time.start()
+	attacking = true
+
+func _on_attack_time_timeout() -> void:
+	net_attack_area.hide()
+	net_collider.disabled = true
+	attacking = false
+
+func _on_net_attack_area_body_entered(body: Node2D) -> void:
+	print(body)
