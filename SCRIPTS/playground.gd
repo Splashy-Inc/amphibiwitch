13a2a1d2b429872a10da2d5c87b5d6376eca86aa
ChyRoseBun <chyrose1927@gmail.com>
2025-05-18 09:24:03 -0400

    tilemap

--- SCRIPTS/playground.gd
+++ SCRIPTS/playground.gd
@@ -3,6 +3,5 @@ extends Level
 @onready var world_light: DirectionalLight2D = $WorldLight
 
 func _level_ready():
-	world_light.energy = .1
 	await get_tree().create_timer(1).timeout
 	Globals.update_info("Updated")
