8fd738909f3ecf8f34b3a86a080bcc81dc6cc9e0
avelitj12 <avelitj12@gmail.com>
2025-05-21 12:51:56 -0400

    Controller support and mobile rotation improvement

--- SCRIPTS/player.gd
+++ SCRIPTS/player.gd
@@ -21,17 +21,21 @@ func _physics_process(delta: float) -> void:
 		if Globals.is_mobile and Globals.joystick:
 			new_direction = Globals.joystick.direction
 		else:
-			new_direction = Input.get_vector("left", "right", "up", "down")
+			new_direction = Input.get_vector("left", "right", "up", "down") + Input.get_vector("stick_left", "stick_right", "stick_up", "stick_down")
 		
 		if new_direction and new_direction != Vector2.ZERO:
 			speed_mod = 1.0
-			direction = direction.lerp(new_direction.normalized(), .1)
+			if Globals.is_mobile:
+				direction = new_direction
+			else:
+				direction = direction.lerp(new_direction.normalized(), .1)
 			animation_player.play("move")
-			rotation = -direction.angle_to(Vector2.UP)
 		else:
 			speed_mod = 0.0
 			animation_player.play("idle")
 		
+		rotation = -direction.angle_to(Vector2.UP)
+		#rotation = -global_position.direction_to(get_global_mouse_position()).angle_to(Vector2.UP)
 	velocity = direction * SPEED * speed_mod
 		
 	move_and_slide()
