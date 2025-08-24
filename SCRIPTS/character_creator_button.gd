4a7c63eff6a98aad08cef9a7481ab915dbc8dcb3
avelitj12 <avelitj12@gmail.com>
2025-08-24 15:59:15 -0400

    Make appearance updates and book work properly

--- SCRIPTS/character_creator_button.gd
+++ SCRIPTS/character_creator_button.gd
@@ -21,6 +21,16 @@ func set_texture(new_texture: Texture2D):
 func set_attribute_data(new_attribute_data: AttributeData):
 	attribute_data = new_attribute_data
 	set_texture(attribute_data.sprite_sheet)
+	match attribute_data.category:
+		AttributeData.Category.SHOES:
+			sprite.hframes = 8
+		AttributeData.Category.SKINTONE:
+			sprite.hframes = 1
+		AttributeData.Category.HAIR:
+			sprite.hframes = 4
+		AttributeData.Category.ACCESSORY:
+			sprite.hframes = 4
+			sprite.vframes = 2
 
 func _on_pressed() -> void:
 	AppearanceEvents.attribute_change_requested.emit(attribute_data)
