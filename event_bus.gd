92b6390cff2fe57b83eb8b8f794233bb1b83168e
avelitj12 <avelitj12@gmail.com>
2025-09-20 14:01:33 -0400

    Set up placeholder inventory UI

--- event_bus.gd
+++ event_bus.gd
@@ -3,3 +3,4 @@ extends Node
 signal menu_requested(menu_name: String)
 signal screen_requested(location_name: LocationData.Name)
 signal player_exited_screen
+signal inventory_updated(data: InventoryData)
